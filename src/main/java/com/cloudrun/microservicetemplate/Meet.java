dbf861e234de8a7642b906ac3b5fd1d6c5d6506e
Begonia4 <rebekah4adventure@gmail.com>
2025-02-26 13:25:22 -0600

    first commit

---
+++ src/main/java/com/cloudrun/microservicetemplate/Meet.java
@@ -0,0 +1,30 @@
+package com.cloudrun.microservicetemplate;
+
+
+// Get the input for a meet class
+public class Meet {
+    private String url;
+    private String schoolName;
+
+    // Getters and Setters
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+    
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+}
+   
+    
+
---
+++ src/main/java/com/cloudrun/microservicetemplate/sqlData.java
@@ -0,0 +1,19 @@
+package com.cloudrun.microservicetemplate;
+
+
+public record sqlData(
+    String name,
+    String meetName,
+    String place,
+    String eventName,
+    String round,
+    String roundName,
+    String heat,
+    String mark,
+    String divisionName,
+    String gender,
+    String genderName,
+    String gradYear
+) {}
+    
+
