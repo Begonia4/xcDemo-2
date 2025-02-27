0fb8ae31c72d0138b8707956cb101af115d71e95
Begonia4 <rebekah4adventure@gmail.com>
2025-02-26 19:42:43 -0600

    secrets

--- src/main/java/com/cloudrun/microservicetemplate/CloudSqlConnectionPoolFactory.java
+++ src/main/java/com/cloudrun/microservicetemplate/CloudSqlConnectionPoolFactory.java
@@ -5,10 +5,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 
 public class CloudSqlConnectionPoolFactory {
-  private static final String INSTANCE_CONNECTION_NAME = "tactile-oxygen-449819-u9:us-central1:mtui-24-k78";
-  private static final String DB_USER = "root";
-  private static final String DB_PASS = "s4H&Apb1xe9";
-  private static final String DB_NAME = "helloworld";
 
   public static DataSource createConnectionPool() {
     HikariConfig config = new HikariConfig();
