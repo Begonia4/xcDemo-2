a3c6f014bece89e3f6b02871dbb2436ba76d7b5c
Rebekah Rutledge <rebekah4adventure@gmail.com>
2025-03-20 18:09:57 -0500

    w4

--- src/main/java/com/cloudrun/microservicetemplate/CloudSqlConnectionPoolFactory.java
+++ src/main/java/com/cloudrun/microservicetemplate/CloudSqlConnectionPoolFactory.java
@@ -12,7 +12,7 @@ public class CloudSqlConnectionPoolFactory {
     config.setUsername("root");
     config.setPassword("V!s4f@cA0(6");
     config.addDataSourceProperty("socketFactory","com.google.cloud.sql.mysql.SocketFactory");
-    config.addDataSourceProperty("cloudSqlInstance", "neon-lock-454313-h0:us-south1:th34msrv");
+    config.addDataSourceProperty("cloudSqlInstance", "neon-lock-454313-h0:us-central1:th34msrx");
 
     return new HikariDataSource(config);
   }
