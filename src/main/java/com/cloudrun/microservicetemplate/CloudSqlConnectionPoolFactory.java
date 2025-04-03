6c6691c588109b39a40f11eaf7d2a9a4745ba809
Begonia4 <rebekah4adventure@gmail.com>
2025-04-03 10:36:54 -0500

    css

--- src/main/java/com/cloudrun/microservicetemplate/CloudSqlConnectionPoolFactory.java
+++ src/main/java/com/cloudrun/microservicetemplate/CloudSqlConnectionPoolFactory.java
@@ -7,6 +7,11 @@ import javax.sql.DataSource;
 public class CloudSqlConnectionPoolFactory {
 
   public static DataSource createConnectionPool() {
+    String DB_NAME = "hello";
+    String DB_USER = "hello";
+    String DB_PASS = "hello";
+    String INSTANCE_CONNECTION_NAME = "hello";
+     
     HikariConfig config = new HikariConfig();
     config.setJdbcUrl(String.format("jdbc:mysql:///" + DB_NAME));
     config.setUsername(DB_USER);
