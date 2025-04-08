2a5470307ebc7530b345d282806b8f13093e5563
Begonia4 <rebekah4adventure@gmail.com>
2025-04-08 13:03:06 -0500

    YAY HAPPY HAPPY HAPPY

--- src/main/java/com/cloudrun/microservicetemplate/CloudSqlConnectionPoolFactory.java
+++ src/main/java/com/cloudrun/microservicetemplate/CloudSqlConnectionPoolFactory.java
@@ -10,7 +10,7 @@ public class CloudSqlConnectionPoolFactory {
     String DB_NAME = "helloworld";
     String DB_USER = "root";
     String DB_PASS = "V!s4f@cA0(6";
-    String INSTANCE_CONNECTION_NAME = "neon-lock-454313-h0:us-central1:th34msr";
+    String INSTANCE_CONNECTION_NAME = "neon-lock-454313-h0:us-central1:th34msrx";
      
     HikariConfig config = new HikariConfig();
     config.setJdbcUrl(String.format("jdbc:mysql:///" + DB_NAME));
